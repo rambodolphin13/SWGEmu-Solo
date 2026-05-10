348f61c24b935f3a997e3db6d1353026b94a087f
rambodolphin13 <natex_69@hotmail.com>
2026-05-10 10:47:55 -0400

    Create stock baseline with perfect startup resources

--- MMOCoreORB/bin/conf/config.lua
+++ MMOCoreORB/bin/conf/config.lua
@@ -147,7 +147,7 @@ Core3 = {
 	},
 
 	------TRE config------
-	TrePath = "/home/swgemu/Desktop/SWGEmu",
+	TrePath = "/home/swgemu/tre",
 	TreFiles = {
 		"default_patch.tre",
 		"patch_sku1_14_00.tre",
